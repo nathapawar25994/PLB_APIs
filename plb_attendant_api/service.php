858dc87f149a387007123490dac48902db3e064f
Natha Pawar <natha.pawar@deltabee.com>
2017-04-17 11:33:32 +0530

    PLB Attendant APIs

--- plb_attendant_api/service.php
+++ plb_attendant_api/service.php
@@ -24,7 +24,7 @@ $app->get('/requestedparking/:input', function($input) {
                 jsonResponse(200,$response);
             }    
         });
-
+//attendant List API
 $app->get('/attendantlist/:input', function($input) {
            
            $response = array();
